873d1975f36a849e1d9839395e128f6e01d53f8c
hasan.bilgin <hbilgin@gtu.edu.tr>
2020-11-22 21:05:10 +0300

    car search test

--- src/test/java/com/hasanbilgin/carsearch/controller/CarSearchControllerTest.java
+++ src/test/java/com/hasanbilgin/carsearch/controller/CarSearchControllerTest.java
@@ -2,29 +2,33 @@ package com.hasanbilgin.carsearch.controller;
 
 import com.hasanbilgin.carsearch.model.Car;
 import com.hasanbilgin.carsearch.service.CarService;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(value = CarSearchController.class)
-@WithMockUser
+import java.util.List;
 public class CarSearchControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private CarService carService;
+    private final CarService carService = new CarService();
 
     @Test
     public void controlBrand() throws Exception {
+        List<Car> carList = carService.search("marka","BMC");
+        Assert.assertEquals(0,carList.stream().filter(car -> !car.getBrand().equals("BMC")).count());
     }
+
+    @Test
+    public void controlModel() throws Exception {
+        List<Car> carList = carService.search("model","A3");
+        Assert.assertEquals(0,carList.stream().filter(car -> !car.getModel().equals("A3")).count());
+    }
+    @Test
+    public void controlType() throws Exception {
+        List<Car> carList = carService.search("sınıf","Pick-up");
+        Assert.assertEquals(0,carList.stream().filter(car -> !car.getType().contains("Pick-up")).count());
+    }
+    @Test
+    public void controlAll() throws Exception {
+        List<Car> carList = carService.search("hepsi","i");
+        Assert.assertEquals(9,carList.size());
+    }
+
 }
